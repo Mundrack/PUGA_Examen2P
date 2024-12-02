ee41580de7325261c49e0e00d0ecf285d773eb3f
laboratorio <laboratorio@UPEAULA-31724>
2024-12-02 09:28:34 -0500

    Agregar archivos de proyecto.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExamenIIMateoPuga
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
